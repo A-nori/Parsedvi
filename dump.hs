20c9a6f4dc6c1690a9a38a6e1fc5368a79a1977c
A-nori <anori@is.s.u-tokyo.ac.jp>
2019-04-09 18:57:23 +0900

    mainをpagesに

--- dump.hs
+++ dump.hs
@@ -24,16 +24,16 @@ getPostamble_sub :: [Command] -> [Command] -> ([Command], [Command])
 getPostamble_sub acc post@(Post _ _ _ _ _ _ _ _:_) = (reverse acc, post)
 getPostamble_sub acc (com:coms) = getPostamble_sub (com : acc) coms
 
--- [Command] to (preamble, main, postamble)
+-- [Command] to (preamble, pages, postamble)
 separate :: [Command] -> (Command, [Command], [Command])
 separate coms =
   let 
     (pre, rest) = getPreamble coms
   in
   let 
-    (main, post) = getPostamble rest
+    (pages, post) = getPostamble rest
   in
-    (pre, main, post)
+    (pre, pages, post)
 
 
 byMeter :: (Int, Int, Int) -> Int -> Double
@@ -49,29 +49,39 @@ convertMeter2Point v = v * 7227 / 2.54
 dumpCommand :: (Int, Int, Int) -> Command -> IO ()
 dumpCommand _ (Set_Char c) = putStrLn $ "set_char " ++ [(toEnum $ fromEnum c)]
 dumpCommand _ (Set _ c) = putStrLn $ "set " ++ (show c)
-dumpCommand (num, den, mag) (Set_Rule a b) = putStrLn $ "set_rule " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
+dumpCommand (num, den, mag) (Set_Rule a b) = 
+  putStrLn $ "set_rule " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) 
+          ++ " " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
 dumpCommand _ (Put _ c) = putStrLn $ "put " ++ (show c)
-dumpCommand (num, den, mag) (Put_Rule a b) = putStrLn $ "put_rule " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
+dumpCommand (num, den, mag) (Put_Rule a b) = 
+  putStrLn $ "put_rule " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a)
+          ++ " " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
 dumpCommand _ Nop = putStrLn "nop"
 dumpCommand _ (Bop cs p) = putStrLn $ "bop" ++ (show cs) ++ " " ++ (show p)
 dumpCommand _ Eop = putStrLn "eop"
 dumpCommand _ Push = putStrLn "push"
 dumpCommand _ Pop = putStrLn "pop"
-dumpCommand (num, den, mag) (Command.Right _ b) = putStrLn $ "right : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
+dumpCommand (num, den, mag) (Command.Right _ b) = 
+  putStrLn $ "right : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
 dumpCommand _ W0 = putStrLn "w0"
-dumpCommand (num, den, mag) (W _ b) = putStrLn $ "w : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
+dumpCommand (num, den, mag) (W _ b) = 
+  putStrLn $ "w : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
 dumpCommand _ X0 = putStrLn "x0"
-dumpCommand (num, den, mag) (X _ b) = putStrLn $ "x : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
-dumpCommand (num, den, mag) (Down _ a) = putStrLn $ "down : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " pt"
+dumpCommand (num, den, mag) (X _ b) = 
+  putStrLn $ "x : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) b) ++ " pt"
+dumpCommand (num, den, mag) (Down _ a) = 
+  putStrLn $ "down : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " pt"
 dumpCommand _ Y0 = putStrLn "y0"
-dumpCommand (num, den, mag) (Y _ a) = putStrLn $ "y : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " pt"
+dumpCommand (num, den, mag) (Y _ a) = 
+  putStrLn $ "y : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " pt"
 dumpCommand _ Z0 = putStrLn "z0"
-dumpCommand (num, den, mag) (Z _ a) = putStrLn $ "z : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " pt"
+dumpCommand (num, den, mag) (Z _ a) = 
+  putStrLn $ "z : " ++ (show $ convertMeter2Point $ byMeter (num, den, mag) a) ++ " pt"
 dumpCommand _ (Fnt_Num f) = putStrLn $ "fnt_num : " ++ (show f)
 dumpCommand _ (Fnt _ k) = putStrLn $ "fnt : " ++ (show k)
 dumpCommand _ (Xxx _ _ x) = putStrLn $ "xxx : " ++ x
 dumpCommand _ (Fnt_Def _ k c s d a l n) = do
-  putStrLn $ "fnt_def : " ++ n
+  putStrLn $ "fnt_def : fnt " ++ (show k) ++ " = " ++ n
 dumpCommand _ (Pre i n d m _ x) = do
   putStrLn $ "dvi version : " ++ (show i)
   putStrLn $ "numerator : " ++ (show n)
@@ -88,24 +98,24 @@ dumpPre (pre@(Pre _ n d m _ _)) = do
 
 
 
--------- dump main
-dumpMain :: (Int, Int, Int) -> [Command] -> Int -> IO ()
-dumpMain _ [] _ = return ()
+-------- dump pages
+dumpPages :: (Int, Int, Int) -> [Command] -> Int -> IO ()
+dumpPages _ [] _ = return ()
 -- Push
-dumpMain (num, den, mag) (Push:coms) n = do
+dumpPages (num, den, mag) (Push:coms) n = do
   putIndent n
   dumpCommand (num, den, mag) Push
-  dumpMain (num, den, mag) coms (n + 1)
+  dumpPages (num, den, mag) coms (n + 1)
 -- Pop
-dumpMain (num, den, mag) (Pop:coms) n = do
+dumpPages (num, den, mag) (Pop:coms) n = do
   putIndent (n - 1)
   dumpCommand (num, den, mag) Pop
-  dumpMain (num, den, mag) coms (n - 1)
+  dumpPages (num, den, mag) coms (n - 1)
 -- otherwise
-dumpMain (num, den, mag) (com:coms) n = do
+dumpPages (num, den, mag) (com:coms) n = do
   putIndent n
   dumpCommand (num, den, mag) com
-  dumpMain (num, den, mag) coms n
+  dumpPages (num, den, mag) coms n
 
 -------- dump postamble
 dumpPost :: [Command] -> IO ()
@@ -120,16 +130,16 @@ dumpPost ((post@(Post_Post q i)):coms) = do
   putStrLn $ show post
   dumpPost coms
 
------- dumpCommands 
+------ dump Commands 
 dumpCommands :: [Command] -> IO ()
 dumpCommands coms = do
   let 
-    (pre, main, post) = separate coms
+    (pre, pages, post) = separate coms
   putStrLn "----- preamble -----"
   (num, den, mag) <- dumpPre pre
   putChar '\n'
-  putStrLn "----- main -----"
-  dumpMain (num, den, mag) main 0
+  putStrLn "----- pages -----"
+  dumpPages (num, den, mag) pages 0
   putChar '\n'
   putStrLn "----- postamble -----"
   dumpPost post
